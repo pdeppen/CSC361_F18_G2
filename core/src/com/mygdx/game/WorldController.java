3760641fb8840218db9aeb108e4ce36f9e6f42e1
Philip Deppen <pdeppen17@aol.com>
2018-09-05 20:00:17 -0400

    finished adding to p115_WC

---
+++ core/src/com/mygdx/game/WorldController.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+/** will need to create package for this */
+
+public class WorldController {
+	
+	private static final String TAG = 
+		WorldController.class.getName();
+	
+	/**
+	 * constructor 
+	 */
+	public WorldController() {
+		
+	}
+	
+	/**
+	 * internal init (useful when reseting an object) 
+	 */
+	public void init() {
+		
+	}
+	
+	/**
+	 * contains game logic
+	 * called several hundred times per sec
+	 * @param deltaTime
+	 */
+	public void update(float deltaTime) {
+		
+	}
+}
