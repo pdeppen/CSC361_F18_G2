18b763357e7857859b1e67e206b63d4871f4c466
Philip Deppen <pdeppen17@aol.com>
2018-09-11 11:09:55 -0400

    finished adding to p149Assets

--- core/src/com/packtpub/libgdx/canyonbunny/Assets.java
+++ core/src/com/packtpub/libgdx/canyonbunny/Assets.java
@@ -1,5 +1,12 @@
 package com.packtpub.libgdx.canyonbunny;
 
-public class Assets {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
 
+public class Assets implements Disposable, AssetErrorListener {
+	
 }
