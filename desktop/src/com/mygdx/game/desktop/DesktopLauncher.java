b81e73473700197750009fde5df26c166bab4854
Owen Burnham <ob0795@cs.ship.edu>
2018-09-13 12:38:52 -0400

    changes to p145_main

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 /** 
  * Group 2
+ * edited by Owen Burnham in Assignment 3
  */
 package com.mygdx.game.desktop;
 
@@ -7,12 +8,40 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.
+Settings;
 
-public class DesktopLauncher 
+/**
+ * edited by Owen Burnham in Assignment 3
+ * This is the main class for the CanyonBunny-Desktop project 
+ */
+public class DesktopLauncher
 {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
+	/**
+	 * Edited by Owen Burnham in Assignment 3
+	 * This is the main method that runs the project
+	 * @param arg
+	 */
 	public static void main (String[] arg) 
 	{
+		if (rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "../CanyonBunny-android/assets/images", "canyonbunny.pack");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "CanyonBunny";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
