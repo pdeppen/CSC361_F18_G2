d367d4023d04bb58ddc6adaa3a58cb9352d6fc05
Tyler <tm8060@ship.edu>
2018-09-14 00:04:22 -0400

    Tyler: Code for 179-180 of level class

--- core/src/com/packtpub/libgdx/canyonbunny/Level.java
+++ core/src/com/packtpub/libgdx/canyonbunny/Level.java
@@ -13,9 +13,9 @@ package com.packtpub.libgdx.canyonbunny;
 
 
 /* @author: Tyler Major
- * pg 177-178 level code
+ * pg 177-178 level code.. also 179-180...also 181-182
  * This class is the loader that will read and interpret the image data
- * 
+ * All clouds, mountains, and waterOverlay are filled in level class
  */
 public class Level 
 {
@@ -56,7 +56,90 @@ public class Level
 		{
 			init(filename);
 		}
-		private void init (String filename) {}
+		private void init (String filename) 
+		{
+			// objects
+			rocks = new Array<Rock>();
+			
+			// load image file that represents the level data
+			Pixmap pixmap = new Pixmap(Gdx.files.internal(filename));
+			
+			// scan pixels from top-left to bottom-right
+			int lastPixel = -1;
+			for (int pixelY = 0; pixelY < pixmap.getHeight(); pixelY++) 
+			{
+				for (int pixelX = 0; pixelX < pixmap.getWidth(); pixelX++) 
+				{
+				AbstractGameObject obj = null;
+				float offsetHeight = 0;
+				// height grows from bottom to top
+				float baseHeight = pixmap.getHeight() - pixelY;
+				// get color of current pixel as 32-bit RGBA value
+				int currentPixel = pixmap.getPixel(pixelX, pixelY);
+				// find matching color value to identify block type at (x,y)
+				// point and create the corresponding game object if there is
+				// a match
+				// empty space
+				if (BLOCK_TYPE.EMPTY.sameColor(currentPixel)) 
+				{
+					// do nothing
+				}
+			// rock
+			else if (BLOCK_TYPE.ROCK.sameColor(currentPixel)) 
+			{
+				if (lastPixel != currentPixel) 
+				{
+				obj = new Rock();
+				float heightIncreaseFactor = 0.25f;
+				offsetHeight = -2.5f;
+				obj.position.set(pixelX, baseHeight * obj.dimension.y
+				* heightIncreaseFactor + offsetHeight);
+				rocks.add((Rock)obj);
+				} 
+				else
+				{
+					rocks.get(rocks.size - 1).increaseLength(1);
+				}
+			}
+			// player spawn point
+			else if
+			(BLOCK_TYPE.PLAYER_SPAWNPOINT.sameColor(currentPixel)) 
+			{
+				
+			}
+			// feather
+			else if (BLOCK_TYPE.ITEM_FEATHER.sameColor(currentPixel)) 
+			{
+				
+			}
+			// gold coin
+			else if (BLOCK_TYPE.ITEM_GOLD_COIN.sameColor(currentPixel)) 
+			{
+				
+			}
+			// unknown object/pixel color
+			else {
+				int r = 0xff & (currentPixel >>> 24); //red color channel
+				int g = 0xff & (currentPixel >>> 16); //green color channel
+				int b = 0xff & (currentPixel >>> 8); //blue color channel
+				int a = 0xff & currentPixel; //alpha channel
+				Gdx.app.error(TAG, "Unknown object at x<" + pixelX + "> y<"
+				+ pixelY + ">: r<" + r+ "> g<" + g + "> b<" + b + "> a<" + a + ">");
+			}
+			lastPixel = currentPixel;
+		}
+	}
+			// decoration
+			clouds = new Clouds(pixmap.getWidth());
+			clouds.position.set(0, 2);
+			mountains = new Mountains(pixmap.getWidth());
+			mountains.position.set(-1, -1);
+			waterOverlay = new WaterOverlay(pixmap.getWidth());
+			waterOverlay.position.set(0, -3.75f);
+			// free memory
+			pixmap.dispose();
+			Gdx.app.debug(TAG, "level '" + filename + "' loaded");
+		}
 		
 		public void render (SpriteBatch batch) {}
 		
