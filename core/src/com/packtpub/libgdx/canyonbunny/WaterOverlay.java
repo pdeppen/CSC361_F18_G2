21b7b5d89b63340dc21e5316919c4e8e1260ff3f
Owen Burnham <ob0795@cs.ship.edu>
2018-09-14 13:19:58 -0400

    changes to p174_waterOverlay

--- core/src/com/packtpub/libgdx/canyonbunny/WaterOverlay.java
+++ core/src/com/packtpub/libgdx/canyonbunny/WaterOverlay.java
@@ -11,5 +11,44 @@ import com.packtpub.libgdx.canyonbunny.game.Assets;
  */
 public class WaterOverlay extends AbstractGameObject 
 {
+	private TextureRegion regWaterOverlay;
+	private float length;
 	
+	/**
+	 * @param length
+	 * Makes an instance of WaterOverlay with
+	 * it's length
+	 */
+	public WaterOverlay (float length) 
+	{
+		this.length = length;
+		init();
+	}
+	
+	/**
+	 * initializes the water overlay's dimensions
+	 */
+	private void init() 
+	{
+		dimension.set(length * 10, 3);
+		
+		regWaterOverlay = Assets.instance.levelDecoration.waterOverlay;
+		
+		origin.x = -dimension.x / 2;
+	}
+	
+	@Override
+	/**
+	 * @param batch
+	 * renders/draws the appropriate batch for the
+	 * water overlay
+	 */
+	public void render (SpriteBatch batch) 
+	{
+		TextureRegion reg = null;
+		reg = regWaterOverlay;
+		batch.draw(reg.getTexture(), position.x + origin.x, position.y + origin.y, origin.x, 
+				origin.y, dimension.x, dimension.y,  scale.x, scale.y, rotation, reg.getRegionX(), 
+				reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+	}
 }
