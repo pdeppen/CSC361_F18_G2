edf43783724505eea237a7bfc5b7abaa1989dc00
Tyler <tm8060@ship.edu>
2018-09-14 00:44:39 -0400

    Tyler: pg_191 added lives and current score to GUI

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -4,6 +4,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.packtpub.libgdx.canyonbunny.Assets;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
@@ -70,6 +71,35 @@ public class WorldRenderer implements Disposable
 		batch.begin();
 		worldController.level.render(batch);
 		batch.end();
+	}
+	
+	//Tyler: This code adds Gui score to top left of screen
+	private void renderGuiScore (SpriteBatch batch) 
+	{
+		float x = -15;
+		float y = -15;
+		batch.draw(Assets.instance.goldCoin.goldCoin,
+		x, y, 50, 50, 100, 100, 0.35f, -0.35f, 0);
+		Assets.instance.fonts.defaultBig.draw(batch,
+		"" + worldController.score,
+		x + 75, y + 37);
+	}
+	
+	//Tyler: This code adds three bunny heads to indicate the lives in the GUI
+	private void renderGuiExtraLive (SpriteBatch batch)
+	{
+		float x = cameraGUI.viewportWidth - 50 -
+		Constants.LIVES_START * 50;
+		float y = -15;
+		
+		for (int i = 0; i < Constants.LIVES_START; i++) 
+		{
+		if (worldController.lives <= i)
+			batch.setColor(0.5f, 0.5f, 0.5f, 0.5f);
+			batch.draw(Assets.instance.bunny.head,
+			x + i * 50, y, 50, 50, 120, 100, 0.35f, -0.35f, 0);
+			batch.setColor(1, 1, 1, 1);
+		}
 	}
 	
 	
