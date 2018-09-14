ea4b457aa7029c2e5aa0a603511b2a0722e05d7e
Owen Burnham <ob0795@cs.ship.edu>
2018-09-14 12:51:28 -0400

    changes to p173_wateroverlay

---
+++ core/src/com/packtpub/libgdx/canyonbunny/WaterOverlay.java
@@ -0,0 +1,15 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+/**
+ * @author Owen Burnham (Assignment 4)
+ * The water overlay class will consist of an image that 
+ * overlays the ground of the whole level
+ */
+public class WaterOverlay extends AbstractGameObject 
+{
+	
+}
