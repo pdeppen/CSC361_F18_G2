de9a48e2a1c7d94acbe7569212e1b175f076b036
Owen Burnham <ob0795@cs.ship.edu>
2018-09-14 13:34:51 -0400

    changes to p183_constants

--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -9,7 +9,8 @@ package com.mygdx.game;
  */
 /**
  * @author Edited by Owen Burnham (Assignment 4)
- *
+ * Holds variables that will always hold the same
+ * value
  */
 public class Constants 
 {
@@ -19,9 +20,18 @@ public class Constants
 	// Visible game world is 5 meters tall
 	public static final float VIEWPORT_HEIGHT = 5.0f;
 	
+	// GUI Width
+	public static final float VIEWPORT_GUI_WIDTH = 800.0f;
+	
+	// GUI Height
+	public static final float VIEWPORT_GUI_HEIGHT = 480.0f;
+	
 	// Location of description file for texture atlas
 	public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack.atlas";
 	
 	// Location of image file for level 01
 	public static final String LEVEL_01 = "levels/level-01.png";
+	
+	// Amount of extra lives at level start
+	public static final int LIVES_START = 3;
 }
