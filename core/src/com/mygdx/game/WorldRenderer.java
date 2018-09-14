ce2b37cfeb5fa99a558fb56b1583008d23c930b8
Tyler <tm8060@ship.edu>
2018-09-14 00:37:55 -0400

    Tyler: pg189-190 worldRenderer code to add second camera

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -5,12 +5,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+/* @author: Tyler Major
+ * pg 189-190 code
+ * The added code creates a second camera that is specifically set up just to render
+ * the game's GUI.
+ */
 
 public class WorldRenderer implements Disposable 
 {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private WorldController worldController;
+	private OrthographicCamera cameraGUI;
 	
 	public WorldRenderer (WorldController worldController) 
 	{ 
@@ -24,6 +32,11 @@ public class WorldRenderer implements Disposable
 		Constants.VIEWPORT_HEIGHT);
 		camera.position.set(0, 0, 0);
 		camera.update();
+		cameraGUI = new OrthographicCamera(Constants.VIEWPORT_GUI_WIDTH,
+				Constants.VIEWPORT_GUI_HEIGHT);
+				cameraGUI.position.set(0, 0, 0);
+				cameraGUI.setToOrtho(true); // flip y-axis
+				cameraGUI.update();
 	}
 	
 	public void render () 
@@ -36,6 +49,11 @@ public class WorldRenderer implements Disposable
 		camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) *
 				width;
 				camera.update();
+		cameraGUI.viewportHeight = Constants.VIEWPORT_GUI_HEIGHT;
+		cameraGUI.viewportWidth = (Constants.VIEWPORT_GUI_HEIGHT / (float)height) * (float)width;
+		cameraGUI.position.set(cameraGUI.viewportWidth / 2,
+		cameraGUI.viewportHeight / 2, 0);
+		cameraGUI.update();
 	}
 	
 	
