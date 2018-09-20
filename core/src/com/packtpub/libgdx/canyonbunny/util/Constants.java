8fe0fb509daeca5026e1eeda395f9f94b5d97bba
Tyler <tm8060@ship.edu>
2018-09-20 14:15:44 -0400

    Tyler Major: Added pg220 Constant code

--- core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -34,4 +34,7 @@ public class Constants
 	
 	// Amount of extra lives at level start
 	public static final int LIVES_START = 3;
+	
+	// Tyler Added this from pg220. Delay after game over
+	public static final float TIME_DELAY_GAME_OVER = 3;
 }
