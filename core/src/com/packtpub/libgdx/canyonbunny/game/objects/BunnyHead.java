e110cf5f47ddc577102a7da4fb0140dd9d6d681d
Owen Burnham <ob0795@cs.ship.edu>
2018-09-20 13:55:30 -0400

    updates to p201_bunnyhead

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/BunnyHead.java
@@ -0,0 +1,15 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+/**
+ * @author Owen Burnham (Assignment 5)
+ * This class is for the bunny head object.
+ * It is the player's character and consists of only
+ * one image, but has to account for jumping, falling,
+ * and picking up objects
+ */
+public class BunnyHead 
+{
+
+}
