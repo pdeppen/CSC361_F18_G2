8c754635b9ea00cf8dadec5b95c278da981de6c7
Owen Burnham <ob0795@cs.ship.edu>
2018-09-20 12:20:52 -0400

    updating p196_abstractgameobject

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
@@ -2,8 +2,10 @@ package com.packtpub.libgdx.canyonbunny.game.objects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
 /**
  * @author Owen Burnham (Assignment 4)
+ * edited by Owen Burnham (Assignment 5)
  * This class will hold all the common attributes
  * and fucntionalities that each of our game objects
  * inherit from
@@ -15,8 +17,15 @@ public abstract class AbstractGameObject
 	public Vector2 origin;
 	public Vector2 scale;
 	public float rotation;
+	public Vector2 velocity;
+	public Vector2 terminalVelocity;
+	public Vector2 friction;
+	
+	public Vector2 acceleration;
+	public Rectangle bounds;
 	
 	/**
+	 * edited by Owen Burnham (Assignment 5)
 	 * Makes instances of all the variables for the 
 	 * abstract game object
 	 */
@@ -27,6 +36,11 @@ public abstract class AbstractGameObject
 		origin = new Vector2();
 		scale = new Vector2(1, 1);
 		rotation = 0;
+		velocity = new Vector2();
+		terminalVelocity = new Vector2(1, 1);
+		friction = new Vector2();
+		acceleration = new Vector2();
+		bounds = new Rectangle();
 	}
 	
 	/**
