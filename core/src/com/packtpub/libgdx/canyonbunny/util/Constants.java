fe08c37b41173e251225366c552e461353630be6
Tyler <tm8060@ship.edu>
2018-09-27 00:09:30 -0400

    Tyler Major: Added pg240 to Constants

--- core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -40,4 +40,16 @@ public class Constants
 	
 	// Philip added this from p206. Sets time limit for feather power up
 	public static final float ITEM_FEATHER_POWERUP_DURATION = 9;
+	
+	//Tyler Added this from pg 240. These set up the buttons for the
+	//main menu
+	public static final String TEXTURE_ATLAS_UI =
+			"images/canyonbunny-ui.pack";
+	public static final String TEXTURE_ATLAS_LIBGDX_UI =
+			"images/uiskin.atlas";
+			// Location of description file for skins
+	public static final String SKIN_LIBGDX_UI =
+			"images/uiskin.json";
+	public static final String SKIN_CANYONBUNNY_UI =
+			"images/canyonbunny-ui.json";
 }
