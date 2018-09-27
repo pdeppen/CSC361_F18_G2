917bc010a5cd5d827afce47bb16b4ff4ef05a920
Owen Burnham <ob0795@cs.ship.edu>
2018-09-27 15:12:49 -0400

    updates to p253_CharacterSkin

--- core/src/com/packtpub/libgdx/canyonbunny/util/CharacterSkin.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/CharacterSkin.java
@@ -13,6 +13,27 @@ public enum CharacterSkin {
 	
 	private String name;
 	private Color color = new Color();
+	private CharacterSkin (String name, float r, float g, float b) {
+		this.name = name;
+		color.set(r, g, b, 1.0f);
+	}
 	
+	/**
+	 * Made by Owen Burnham (Assignment 6)
+	 * All character skins are defined using a name
+	 * that is used for display and RGB color values
+	 */
+	@Override
+	public String toString () {
+		return name;
+	}
+	
+	/**
+	 * Made by Owen Burnham (Assignment 6)
+	 * @return the color value used to describe a color
+	 */
+	public Color getColor () {
+		return color;
+	}
 
 }
