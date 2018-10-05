7c154b0669e43a96ab7b4e8065b3944d9b345632
pdeppen <pdeppen17@aol.com>
2018-10-05 14:25:24 -0400

    finished adding to p323AudioManager

--- core/src/com/packtpub/libgdx/canyonbunny/util/AudioManager.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/AudioManager.java
@@ -14,14 +14,18 @@ public class AudioManager {
 	private Music playingMusic;
 	
 	// singleton: prevent instantiation from other classes
-	private AudioManager() {}
+	private AudioManager() 
+	{
+		
+	}
 	
 	/**
 	 * Made by Philip Deppen (Assignment 8, p.322)
 	 * Overloaded this method to make certain paramters optional
 	 * @param sound
 	 */
-	public void play (Sound sound) {
+	public void play (Sound sound) 
+	{
 		play (sound, 1);
 	}
 	
@@ -31,7 +35,8 @@ public class AudioManager {
 	 * @param sound
 	 * @param volume
 	 */
-	public void play (Sound sound, float volume) {
+	public void play (Sound sound, float volume) 
+	{
 		play (sound, volume, 1);
 	}
 	
@@ -42,7 +47,8 @@ public class AudioManager {
 	 * @param volume
 	 * @param pitch
 	 */
-	public void play (Sound sound, float volume, float pitch) {
+	public void play (Sound sound, float volume, float pitch)
+	{
 		play (sound, volume, pitch, 0);
 	}
 	
@@ -54,12 +60,62 @@ public class AudioManager {
 	 * @param pitch
 	 * @param pan
 	 */
-	public void play (Sound sound, float volume, float pitch, float pan) {
+	public void play (Sound sound, float volume, float pitch, float pan) 
+	{
 		if (!GamePreferences.instance.sound)
 			return;
 		
 		sound.play(GamePreferences.instance.volSound * volume, pitch, pan);
 	}
 	
+	/**
+	 * Made by Philip Deppen (Assignment 8, p.323)
+	 * Another overload play method that takes in the Music that will be played
+	 * @param music
+	 */
+	public void play (Music music) 
+	{	
+		stopMusic();
+		playingMusic = music;
+		if (GamePreferences.instance.music) 
+		{
+			music.setLooping(true);
+			music.setVolume(GamePreferences.instance.volMusic);
+			music.play();
+		}
+	}
+	
+	/**
+	 * Made by Philip Deppen (Assignment 8, p.323)
+	 * Stops music
+	 */
+	public void stopMusic()
+	{
+		if (playingMusic != null)
+			playingMusic.stop();
+	}
+	
+	/**
+	 * Made by Philip Deppen (Assignment 8, p.323)
+	 * Allows the Options menu to inform AudioManager when settings
+	 * have changed to execute appropriate actions.
+	 */
+	public void onSettingsUpdated()
+	{
+		if (playingMusic == null)
+			return;
+		
+		playingMusic.setVolume(GamePreferences.instance.volMusic);
+		
+		if (GamePreferences.instance.music)
+		{
+			if (!playingMusic.isPlaying())
+				playingMusic.play();
+		}
+		else
+		{
+			playingMusic.pause();
+		}
+	}
 	
 }
