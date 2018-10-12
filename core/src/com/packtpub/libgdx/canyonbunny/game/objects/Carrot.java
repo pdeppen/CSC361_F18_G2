2ac56171576c511eb3e0156c630dbbcacdc06a02
Owen Burnham <ob0795@cs.ship.edu>
2018-10-12 12:57:37 -0400

    Changes to p340_Carrot

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Carrot.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Carrot.java
@@ -29,6 +29,28 @@ public class Carrot extends AbstractGameObject
 	 */
 	private void init ()
 	{
-
+		dimension.set(0.25f, 0.5f);
+		
+		regCarrot = com.packtpub.libgdx.canyonbunny.util.Assets.instance.levelDecoration.carrot;
+		
+		// Set bounding box for collision detection
+		bounds.set(0, 0, dimension.x, dimension.y);
+		origin.set(dimension.x / 2, dimension.y / 2);
+	}
+	
+	/**
+	 * Created by Owen Burnham (Assignment 9)
+	 * renders the carrot game object
+	 */
+	public void render (SpriteBatch batch)
+	{
+		TextureRegion reg = null;
+		
+		reg = regCarrot;
+		batch.draw(reg.getTexture(), position.x - origin.x, 
+				position.y - origin.y, origin.x, origin.y, dimension.x, 
+				dimension.y, scale.x, scale.y, rotation, reg.getRegionX(),
+				reg.getRegionY(), reg.getRegionWidth(),
+				reg.getRegionHeight(), false, false);
 	}
 }
