bbf9325e84147073decfa28109571044ce370329
Owen Burnham <ob0795@cs.ship.edu>
2018-10-12 12:45:23 -0400

    Changes to p339_Carrot

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Carrot.java
@@ -0,0 +1,34 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+
+/**
+ * @author Owen Burnham (Assignment 9)
+ * Class for the carrot game object
+ */
+public class Carrot extends AbstractGameObject
+{
+	private TextureRegion regCarrot;
+	
+	/**
+	 * Created by Owen Burnham (Assignment 9)
+	 * runs the carrot through the init() method
+	 */
+	public Carrot()
+	{
+		init();
+	
+	}
+	
+	/**
+	 * Created by Owen Burnham (Assignment 9)
+	 * initializes the carrot appropriately
+	 */
+	private void init ()
+	{
+
+	}
+}
